6adea16b0a234d3d1faefae99bd6a2a2468b2fd2
OlgaTatcher <dai.akemi230318@gmail.com>
2023-09-26 15:45:20 +0300

    added variables to class Order, class CoffeeOrderBoard created methods <add>,<deliver>,<deliverByNumber>,<draw> in <CoffeeOrderBoard>

--- src/coffeeOrder/Order.java
+++ src/coffeeOrder/Order.java
@@ -1,2 +1,37 @@
-package coffeeOrder;public class Order {
+package coffeeOrder;
+
+public class Order {
+    private int orderNumber;
+    private String Client;
+
+    public Order(int orderNumber, String client) {
+        this.orderNumber = orderNumber;
+        Client = client;
+    }
+
+
+    public int getOrderNumber() {
+        return orderNumber;
+    }
+
+    public void setOrderNumber(int orderNumber) {
+        this.orderNumber = orderNumber;
+    }
+
+    public String getClient() {
+        return Client;
+    }
+
+    public void setClient(String client) {
+        Client = client;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "orderNumber=" + orderNumber +
+                ", Client='" + Client + '\'' +
+                '}';
+    }
+
 }
